0630acb18abc5ba7ba5550cf7117d27735e73543
Muhammad Hashir Awaiz <167025341+Muhammad-Hashir-55@users.noreply.github.com>
2025-07-18 17:31:33 +0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
 
 export const metadata: Metadata = {
-  title: 'PDBlend',
+  title: 'PDFBlend',
   description: 'Merge and convert to PDFs with ease.',
 };
 
